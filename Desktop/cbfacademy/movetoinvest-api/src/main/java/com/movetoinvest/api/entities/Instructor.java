4c49eabaebaa6ef0481491832086b6a378e1b029
[al-ninsima] <[al.ninsima@gmail.com]>
2025-09-07 10:38:05 +0100

    update instructor.java to link Instructor to ContributionRule - step to make sure instructors can pick how they want to invest

--- Desktop/cbfacademy/movetoinvest-api/src/main/java/com/movetoinvest/api/entities/Instructor.java
+++ Desktop/cbfacademy/movetoinvest-api/src/main/java/com/movetoinvest/api/entities/Instructor.java
@@ -4,25 +4,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Instructor {
-@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private Long id;
 
     private String name;
+    
     private String email;
 
-    // Constructors
-    public Instructor() {}
-
-    public Instructor(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters & Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -31,4 +34,8 @@ public class Instructor {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    @OneToOne
+    @JoinColumn(name = "default_rule_id")
+    private ContributionRule defaultRule;
 }
